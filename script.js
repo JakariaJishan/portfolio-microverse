c9007af5317f16b2acc3c840f327298dd37974e3
DarkxDev <bryanfernandohurtado@gmail.com>
2022-12-19 16:37:14 -0500

    Add EventListeners

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const crossBtn = document.querySelector('.cross-btn');
 const mobileItems = document.querySelectorAll('.mobile-item');
 
+hamburger.addEventListener('click', ()=>{
+  mobileMenu.style.display="block";
+});
+
+crossBtn.addEventListener('click', ()=>{
+  mobileMenu.style.display = 'none'
+})
+
+for (let i = 0; i < mobileItems.length; i++) {
+  const element = mobileItems[i];
+  element.addEventListener('click', ()=>{
+      mobileMenu.style.display='none'   
+  })
+}
