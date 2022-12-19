29d95eb72dec8682a76edbd0d1b3ec8112fee8b9
JakariaJishan <jakariajishan987@gmail.com>
2022-12-20 03:28:52 +0600

    Add portfolio mobile menu

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const crossBtn = document.querySelector('.cross-btn');
+const mobileItems = document.querySelectorAll('.mobile-item');
+
