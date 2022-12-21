4de394ade61eca7dfb5652f8ced62fdb3ee8e448
JakariaJishan <jakariajishan987@gmail.com>
2022-12-21 23:39:25 +0600

    Add contents dynamically and add  popup

--- script.js
+++ script.js
@@ -18,9 +18,58 @@ for (let i = 0; i < mobileItems.length; i += 1) {
   });
 }
 
-// projects
+// projects-recent work
+let workproject = {
+  id: '_1',
+  name: "Multi-Post Stories",
+  desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
+  featuredImg: "./images/works.png",
+  technologies: ["html","css", "bootstrap", "ruby"],
+  liveLink: "#",
+  sourceLink: "#",
+};
+document.querySelector(".works").innerHTML += `<div class="recent-work">
+<h2>${workproject.name}</h2>
+<div class="line-light"></div>
+</div>
+
+<div class="multi-post">
+<img src="${workproject.featuredImg}" alt="Multi-Post Stories project image">
+<div class="multi-post-content">
+    <h3>${workproject.name}</h3>
+    <p>${workproject.desc}
+    </p>
+    <div class="keywords">
+        <ul>
+            <li>
+                <a href="#">${workproject.technologies[0]}</a>
+            </li>
+            <li>
+            <a href="#">${workproject.technologies[1]}</a>
+            </li>
+            <li>
+            <a href="#">${workproject.technologies[2]}</a>
+            </li>
+            <li>
+            <a href="#">${workproject.technologies[3]}</a>
+            </li>
+        </ul>
+    </div>
+    <button type="submit" class="primary-btn">See Project</button>
+</div>
+</div>`;
+
+document.querySelector('.primary-btn').addEventListener('click', ()=>{
+  let popup = document.querySelector(".popup");
+  popup.style.display = "block";
+  popup.innerHTML = ''
+  getPopupId(workproject.id)
+})
+
+// projects...
 let projectsArr = [
   {
+    id: "_1",
     name: "Profesional Art Printing Data",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
     featuredImg: "images/works.png",
@@ -29,6 +78,25 @@ let projectsArr = [
     sourceLink: "#",
   },
   {
+    id: "_2",
+    name: "Profesional Art Printing Data2",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
+    featuredImg: "images/projects-2.png",
+    technologies: ["html", "bootstrap", "ruby"],
+    liveLink: "#",
+    sourceLink: "#",
+  },
+  {
+    id: "_3",
+    name: "Keeping track of hundreds  of components website",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
+    featuredImg: "images/works.png",
+    technologies: ["html", "bootstrap", "ruby"],
+    liveLink: "#",
+    sourceLink: "#",
+  },
+  {
+    id: "_4",
     name: "Profesional Art Printing Data",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
     featuredImg: "images/works.png",
@@ -37,7 +105,8 @@ let projectsArr = [
     sourceLink: "#",
   },
   {
-    name: "Profesional Art Printing Data",
+    id: "_5",
+    name: "Keeping track of hundreds  of components website",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
     featuredImg: "images/works.png",
     technologies: ["html", "bootstrap", "ruby"],
@@ -45,22 +114,7 @@ let projectsArr = [
     sourceLink: "#",
   },
   {
-    name: "Profesional Art Printing Data",
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
-    featuredImg: "images/works.png",
-    technologies: ["html", "bootstrap", "ruby"],
-    liveLink: "#",
-    sourceLink: "#",
-  },
-  {
-    name: "Profesional Art Printing Data",
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
-    featuredImg: "images/works.png",
-    technologies: ["html", "bootstrap", "ruby"],
-    liveLink: "#",
-    sourceLink: "#",
-  },
-  {
+    id: "_6",
     name: "Profesional Art Printing Data",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has beentheindustry's standard",
     featuredImg: "images/works.png",
@@ -73,53 +127,83 @@ let projectsArr = [
 for (let i = 0; i < projectsArr.length; i++) {
   const element = projectsArr[i];
   let projects = document.getElementById("projects");
+  projects.innerHTML += `<div class="project-background">
+  <div class="project-foreground">
+      <div class="projects-content" id="project-content">
+          <h3>${element.name}</h3>
+          <p>${element.desc}</p>
+      </div>
 
-  let background = document.createElement("div");
-  background.className = "project-background";
-  projects.appendChild(background);
+      <div class="project-keyword">
+          <ul>
 
-  let foreground = document.createElement("div");
-  foreground.className = "project-foreground";
-  background.appendChild(foreground);
+              <li>
+                  <a href="#">${element.technologies[0]}</a>
+              </li>
+              <li>
+                  <a href="#">${element.technologies[1]}</a>
+              </li>
+              <li>
+                  <a href="#">${element.technologies[2]}</a>
+              </li>
+          </ul>
+      </div>
+      <button type="submit" class="card-btn" id="card${element.id}">See Project</button>
+  </div>
+</div>`;
+}
 
-  let content = document.createElement("div");
-  content.className = "projects-content";
-  foreground.appendChild(content);
-
-  let h3 = document.createElement("h3");
-  h3.innerHTML = element.name;
-  content.appendChild(h3);
-  let p = document.createElement("p");
-  p.innerHTML = element.desc;
-  content.appendChild(p);
-
-  let projectKeyword = document.createElement("div");
-  projectKeyword.className = "project-keyword";
-  content.appendChild(projectKeyword);
-
-  let ul = document.createElement("ul");
-  projectKeyword.appendChild(ul);
-
-  for (let j = 0; j < element.technologies.length; j++) {
-    const element2 = element.technologies[j];
-    let li = document.createElement("li");
-    li.innerHTML += `<a href="#">` + element2 + `</a>`;
-    ul.appendChild(li);
-  }
-
-  let cardBtn = document.createElement("button");
-  cardBtn.className = "card-btn";
-  cardBtn.setAttribute("type", "submit");
-  cardBtn.innerHTML = "See Projects";
-  foreground.appendChild(cardBtn);
-
+for (let i = 0; i < projectsArr.length; i++) {
+  let cardBtn = document.querySelector("#card" + projectsArr[i].id);
   cardBtn.addEventListener("click", () => {
-    let popup = document.querySelector(".popup-content");
+    let popup = document.querySelector(".popup");
     popup.style.display = "block";
+    popup.innerHTML = "";
+    getPopupId(projectsArr[i].id);
   });
-  let cancle = document.querySelector(".cancle");
-  cancle.addEventListener('click', ()=>{
-    let popup = document.querySelector(".popup-content");
+}
+
+// popup window
+
+function getPopupId(id) {
+  console.log(id);
+  for (let j = 0; j < projectsArr.length; j++) {
+    if (projectsArr[j].id === id) {
+      let popupWindow = document.getElementById("popup");
+      popupWindow.innerHTML += `<div class="popup-content">
+          <div class="popup-header">
+              <h2>${projectsArr[j].name}</h2>
+              <button class="cancle" id="cancle${projectsArr[j].id}"><img src="./images/Icon - Cancel.png" alt="cancle icon"></button>
+          </div>
+          <ul>
+              <li>
+                  <a href="#">${projectsArr[j].technologies[0]}</a>
+              </li>
+              <li>
+                  <a href="#">${projectsArr[j].technologies[1]}</a>
+              </li>
+              <li>
+                  <a href="#">${projectsArr[j].technologies[2]}</a>
+              </li>
+          </ul>
+          <div class="popup-footer">
+              <img class="popup-img" src="${projectsArr[j].featuredImg}" alt="">
+              <div class="footer-content">
+                  <p>${projectsArr[j].desc}</p>
+                  <div class="popup-btn">
+                      <a href="${projectsArr[j].liveLink}">See live <img src="./images/live.png" alt=""></a>
+                      <a href="${projectsArr[j].sourceLink}">See source <img src="./images/Icon -GitHub.png" alt=""></a>
+                  </div>
+              </div>
+          
+          </div>
+          </div>`;
+    }
+  }
+  let canclex = document.querySelector("#cancle" + id);
+  canclex.addEventListener("click", () => {
+    let popup = document.querySelector(".popup");
     popup.style.display = "none";
-  })
+    console.log("cancle");
+  });
 }
